f02c6472cda453536c838c3f1898c51cefeb812d
Илья Крутько <mr.darkilya@mail.ru>
2020-11-16 20:15:07 +0300

    qr code fix

--- src/pages/qr-codes/index.js
+++ src/pages/qr-codes/index.js
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from "react"
 import {Layout, Card, Spin} from "antd";
-import qrcode from "qrcode-generator"
 import firestore from "../../firestore";
 import Header from "../../components/Header";
 import "./qrcodes.css"
@@ -19,16 +18,22 @@ const QRCodes = () => {
                     querySnapshot.forEach(doc => {
                         items.push({"id": doc.id, ...doc.data()})
                     });
-                    console.log(items);
                     setItems(items);
                     items.forEach(item => {
-                        const typeNumber = 0;
-                        const errorCorrectionLevel = 'Q';
-                        const data = item.id;
-                        const qr = qrcode(typeNumber, errorCorrectionLevel);
-                        qr.addData(data);
-                        qr.make();
-                        document.getElementById(item.id).innerHTML = qr.createImgTag(4);
+                        var QRCode = require('qrcode')
+                        var canvas = document.getElementById(item.id)
+
+                        QRCode.toCanvas(canvas, item.id, function (error) {
+                            if (error) console.error(error)
+                            console.log('success!');
+                        })
+                        // const typeNumber = 0;
+                        // const errorCorrectionLevel = 'Q';
+                        // const data = item.id;
+                        // const qr = qrcode(typeNumber, errorCorrectionLevel);
+                        // qr.addData(data);
+                        // qr.make();
+                        // document.getElementById(item.id).innerHTML = qr.createImgTag(4);
                     })
                 }
             );
@@ -40,7 +45,7 @@ const QRCodes = () => {
             <Header selected={"qr-codes"}/>
             <Content style={{padding: '0 50px'}}>
                 <Layout className="site-layout-background">
-                    <Content style={{padding: '0 24px', minHeight: 480}}>
+                    <Content style={{padding: '0 0', minHeight: 480}}>
                         <Card title="QR коды">
                             {items.length !== 0 ?
                                 items.map(item =>
@@ -48,9 +53,10 @@ const QRCodes = () => {
                                         width: '25%',
                                         textAlign: 'center'
                                     }}>
-                                        <div
-                                            style={{height: 118, width: 118, margin: "0 auto"}}
-                                            id={item.id}/>
+                                        <div style={{height: 118, width: 118, margin: "0 auto"}}>
+                                            <canvas id={item.id}/>
+                                        </div>
+
                                         <span><b>{item.id}</b></span>
                                     </Card.Grid>)
                                 :
